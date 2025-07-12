4410b8d7d619dc8700827d7feb8736410e9f5ea6
pakhrom <124648952+pakhrom@users.noreply.github.com>
2025-07-12 18:56:20 +0700

    chore(web): Configure tailwindcss Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite'; 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwindcss(), svelte()]
+});
