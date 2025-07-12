0af5df4a8cf2e4e520209f8eaf5017fdad15de0a
pakhrom <124648952+pakhrom@users.noreply.github.com>
2025-07-12 19:09:13 +0700

    fix(web): Fix tailwindcss Vite plugin configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss(), svelte()]
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [svelte()]
 });
