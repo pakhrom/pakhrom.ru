bb9f582568803ae59b3e2138d3f6630f19e543cd
pakhrom <124648952+pakhrom@users.noreply.github.com>
2025-08-01 18:13:16 +0700

    fix: update "site" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pakhrom.neocities.org",
+  site: "https://pakhrom.nekoweb.org",
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()]
