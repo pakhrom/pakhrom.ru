8c750ad56e9d0fa87a9de3a6240c72f83754e1e4
pakhrom <124648952+pakhrom@users.noreply.github.com>
2025-08-01 18:14:00 +0700

    fix: remove unused svelte import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite'; 
-import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
