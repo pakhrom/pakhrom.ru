b402410d0a7847fd682c9b7a58e7a75e96d55a61
pakhrom <124648952+pakhrom@users.noreply.github.com>
2025-10-14 19:49:05 +0700

    chore: add svelte to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://pakhrom.ru",
 	trailingSlash: "never",
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [svelte()],
 
 	server: {
 		allowedHosts: [".trycloudflare.com"],
